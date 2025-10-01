30eddb251e68033b818614c5a47f27b440f9b4de
STUDIOFREE99 <libertiarecordings@gmail.com>
2025-10-01 15:31:23 -0600

    [dyad] Moved GTM script to body to fix hydration error - wrote 1 file(s)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,20 +50,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-XXXXXXX'); // TODO: Replace GTM-XXXXXXX with your actual GTM ID
-            `,
-          }}
-        />
         {/* Organization JSON-LD Schema */}
         <Script
           id="json-ld-organization" // Unique ID for the script
@@ -84,6 +70,20 @@ export default function RootLayout({
             style={{ display: 'none', visibility: 'hidden' }}
           ></iframe>
         </noscript>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager-body" // Changed ID to reflect new placement
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-XXXXXXX'); // TODO: Replace GTM-XXXXXXX with your actual GTM ID
+            `,
+          }}
+        />
         <SessionContextProvider>
           {children}
         </SessionContextProvider>
