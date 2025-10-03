832fdea9c428c6edf92d44fbfd2e15de34e4cde1
STUDIOFREE99 <libertiarecordings@gmail.com>
2025-10-03 11:33:53 -0600

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'kithkin',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,8 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'kithkin',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
