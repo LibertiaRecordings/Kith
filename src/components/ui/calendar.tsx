86585497c32964d228de1f6d79dee670f62736fa
STUDIOFREE99 <libertiarecordings@gmail.com>
2025-10-03 00:38:11 -0600

    [dyad] Re-applied calendar icon fix and requested a full rebuild - wrote 1 file(s)

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,10 +54,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }: React.SVGProps<SVGSVGElement>) => ( // Corrected name to IconLeft
+        IconLeft: ({ className, ...props }: React.SVGProps<SVGSVGElement>) => (
           <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
         ),
-        IconRight: ({ className, ...props }: React.SVGProps<SVGSVGElement>) => ( // Corrected name to IconRight
+        IconRight: ({ className, ...props }: React.SVGProps<SVGSVGElement>) => (
           <ChevronRight className={cn("h-4 w-4", className)} {...props} />
         ),
       }}
